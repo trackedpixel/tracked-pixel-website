b17b748662e1e7b00080f8915d10b65437772e7f
Brian McCarthy <brian.mcc@rthy.org>
2017-07-07 07:16:29 -0400

    fix url of auth redirect in prod

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,5 +1,5 @@
 export const environment = {
   production: true,
-  authRedirectUri: 'trackedpixel.github.io/tracked-pixel-website/callback',
+  authRedirectUri: 'https://trackedpixel.github.io/tracked-pixel-website/callback',
   apiUrl: 'https://serene-garden-72300.herokuapp.com'
 };
