da2a580c5127ae77894cdfae18bba2d16a1aebf4
Brian McCarthy <brian.mcc@rthy.org>
2017-07-21 19:01:50 -0400

    request permission on page load

--- src/app/desktop-notification.service.ts
+++ src/app/desktop-notification.service.ts
@@ -7,8 +7,33 @@ export class DesktopNotificationService {
   constructor(private pusherService: PusherService) { }
 
   public init() {
+    // Let's check if the browser supports notifications
+    if (!('Notification' in window)) {
+      console.log('This browser does not support notifications.');
+      return;
+    }
+
+    if (Notification['permission'] === 'granted') {
+      this.subscribeToPusherService();
+
+    } else /*if (Notification['permission'] !== 'denied')*/ {
+
+      Notification.requestPermission((permission) => {
+        // Whatever the user answers, we make sure Chrome stores the information
+        if (!('permission' in Notification)) {
+          Notification['permission'] = permission;
+        }
+
+        if (permission === 'granted') {
+          this.subscribeToPusherService();
+        }
+      });
+    }
+  }
+
+  private subscribeToPusherService() {
     this.pusherService.trackings$.subscribe(data => {
-      this.createNotification(data.description);
+      this.sendNotification(data.description);
     });
   }
 
@@ -17,40 +42,4 @@ export class DesktopNotificationService {
     const notification = new Notification('Tracking Pixel Viewed', { body: `${description} has been viewed!` });
     window.navigator.vibrate(500);
   }
-
-  // function for creating the notification
-  private createNotification(description: string) {
-
-    // Let's check if the browser supports notifications
-    if (!('Notification' in window)) {
-      console.log('This browser does not support notifications.');
-      return;
-    }
-
-    // Let's check if the user is okay to get some notification
-    if (Notification['permission'] === 'granted') {
-
-      // If it's okay let's create a notification
-      this.sendNotification(description);
-
-    } else if (Notification['permission'] !== 'denied') {
-
-      // Otherwise, we need to ask the user for permission
-      // Note, Chrome does not implement the permission static property
-      // So we have to check for NOT 'denied' instead of 'default'
-      Notification.requestPermission(function (permission) {
-
-        // Whatever the user answers, we make sure Chrome stores the information
-        if (!('permission' in Notification)) {
-          Notification['permission'] = permission;
-        }
-
-        // If the user is okay, let's create a notification
-        if (permission === 'granted') {
-          this.sendNotification(description);
-        }
-      });
-    }
-  }
-
 }
