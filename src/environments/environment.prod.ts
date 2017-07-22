b55e21a0907ea4859d664661691ad66b889b07b4
Brian McCarthy <brian.mcc@rthy.org>
2017-07-22 14:28:51 -0400

    use https for api

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,8 +1,8 @@
 export const environment = {
   production: true,
   authRedirectUri: 'https://www.trackedpixel.com/callback',
-  pusherAuthUri: 'http://api.trackedpixel.com/pusher/auth',
+  pusherAuthUri: 'https://api.trackedpixel.com/pusher/auth',
   pusherCluster: 'us2',
   pusherKey: '7ade6cf1b661c4fa8637',
-  apiUrl: 'http://api.trackedpixel.com'
+  apiUrl: 'https://api.trackedpixel.com'
 };
